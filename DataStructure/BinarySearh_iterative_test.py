8e8fea7a8d799c24b6462b35b20c49e817fb7e61
bLuEscReEn <theetat.ae@gmail.com>
2025-07-24 01:20:32 +0700

    update

--- DataStructure/BinarySearh_iterative_test.py
+++ DataStructure/BinarySearh_iterative_test.py
@@ -1,200 +1,48 @@
 class Node:
-	def __init__(self, value):
-		self.__value = value
-		self.__prev = None
-		self.__next = None
+	def __init__(self, data):
+		self.__left		= None
+		self.__right	= None
+		self.__data		= data
+  
+	@property
+	def left(self):
+		return self.__left
 
 	@property
-	def value(self):
-		return self.__value
+	def right(self):
+		return self.__right
 
 	@property
-	def next(self):
-		return self.__next
+	def data(self):
+		return self.__data
 
-	@next.setter
-	def next(self, node):
-		self.__next = node
-
-	@property
-	def prev(self):
-		return self.__prev
-
-	@prev.setter
-	def prev(self, node):
-		self.__prev = node
-
-
-class LinkedList:
-	def __init__(self):
-		self.__head = None
-		self.__tail = None
-		self.__size = 0
-
-	@property
-	def size(self):
-		return self.__size
-
-	def __len__(self):
-		return self.__size
-
-	def is_empty(self):
-		return self.__head is None
-
-	def insert_start(self, value):
-		new_node = Node(value)
-		new_node.next = self.__head
-
-		if not self.is_empty():
-			self.__head.prev = new_node
+	@left.setter
+	def left(self, value):
+		self.__left = value
+  
+	@right.setter
+	def right(self, value):
+		self.__right = value
+  
+def searchI(l, h, val, nums):
+    
+	while h > l:
+		m = (l+h)//2
+		if val == nums[m]:
+			return m
+		elif val > nums[m]:
+			l = m + 1
 		else:
-			self.__tail = new_node
+			h = m - 1
+	return None
+nums = [1,3,2,93,2,23,89,433]
+nums.sort()
 
-		self.__head = new_node
-		self.__size += 1
-		return new_node
+print(nums)
 
-	def insert_end(self, value):
-		new_node = Node(value)
+n = 17
+ans = f"found first {n} at index : {searchI(0, len(nums)-1, n, nums)}"
 
-		if self.is_empty():
-			self.__head = self.__tail = new_node
-		else:
-			self.__tail.next = new_node
-			new_node.prev = self.__tail
-			self.__tail = new_node
-
-		self.__size += 1
-		return new_node
-
-	def insert_at(self, value, index):
-		if index < 0 or index > self.__size:
-			raise IndexError("Index out of bounds")
-		if index == 0:
-			return self.insert_start(value)
-		if index == self.__size:
-			return self.insert_end(value)
-
-		new_node = Node(value)
-		mid = self.__size // 2
-
-		if index <= mid:
-			current = self.__head
-			for _ in range(index - 1):
-				current = current.next
-		else:
-			current = self.__tail
-			for _ in range(self.__size - index):
-				current = current.prev
-
-		new_node.next = current.next
-		new_node.prev = current
-		if current.next:
-			current.next.prev = new_node
-		current.next = new_node
-
-		self.__size += 1
-		return new_node
-
-	def remove(self, index):
-		if self.is_empty():
-			raise IndexError("Remove from empty list")
-		if index < 0 or index >= self.__size:
-			raise IndexError("Index out of bounds")
-
-		if index == 0:
-			return self.remove_head()
-		if index == self.__size - 1:
-			return self.remove_tail()
-
-		mid = self.__size // 2
-		if index <= mid:
-			current = self.__head
-			for _ in range(index):
-				current = current.next
-		else:
-			current = self.__tail
-			for _ in range(self.__size - index - 1):
-				current = current.prev
-
-		current.prev.next = current.next
-		current.next.prev = current.prev
-		self.__size -= 1
-		return current.value
-
-	def remove_head(self):
-		if self.is_empty():
-			raise IndexError("Remove from empty list")
-
-		removed_value = self.__head.value
-		if self.__head == self.__tail:
-			self.__head = self.__tail = None
-		else:
-			self.__head = self.__head.next
-			self.__head.prev = None
-
-		self.__size -= 1
-		return removed_value
-
-	def remove_tail(self):
-		if self.is_empty():
-			raise IndexError("Remove from empty list")
-
-		removed_value = self.__tail.value
-		if self.__head == self.__tail:
-			self.__head = self.__tail = None
-		else:
-			self.__tail = self.__tail.prev
-			self.__tail.next = None
-
-		self.__size -= 1
-		return removed_value
-
-	def find(self, value):
-		current = self.__head
-		index = 0
-		while current:
-			if current.value == value:
-				return index
-			current = current.next
-			index += 1
-		return -1
-
-	def __str__(self):
-		values = []
-		current = self.__head
-		while current:
-			values.append(str(current.value))
-			current = current.next
-		return " <-> ".join(values) + " -> None" if values else "Empty list"
-
-
-def main():
-	dll = LinkedList()
-	dll.insert_start(10)
-	dll.insert_end(20)
-	dll.insert_end(30)
-	dll.insert_start(5)
-
-	print("Forward:", dll)
-	print("Size:", dll.size)
-
-	print("\nInsert 'Middle' at index 2")
-	dll.insert_at("Middle", 2)
-	print(dll)
-
-	print("\nRemoved head:", dll.remove_head())
-	print(dll)
-
-	print("\nRemoved tail:", dll.remove_tail())
-	print(dll)
-
-	print("\nRemoved index 1:", dll.remove(1))
-	print(dll)
-
-	print("\nFind index of 20:", dll.find(20))
-	print("Find index of 999:", dll.find(999))
-	print("Length with len():", len(dll))
-
-
-main()
+print(ans)
+  
+  
--- DataStructure/LinkList_Doubly.py
+++ DataStructure/LinkList_Doubly.py
@@ -1,156 +1,172 @@
 class Node:
-    def __init__(self, value):
-        self.__value = value
-        self.__prev = None
-        self.__next = None
+	def __init__(self, value):
+		self.__value = value
+		self.__prev = None
+		self.__next = None
 
-    @property
-    def value(self):
-        return self.__value
+	@property
+	def value(self):
+		return self.__value
 
-    @property
-    def next(self):
-        return self.__next
+	@property
+	def next(self):
+		return self.__next
 
-    @next.setter
-    def next(self, node):
-        self.__next = node
+	@next.setter
+	def next(self, node):
+		self.__next = node
 
-    @property
-    def prev(self):
-        return self.__prev
+	@property
+	def prev(self):
+		return self.__prev
 
-    @prev.setter
-    def prev(self, node):
-        self.__prev = node
+	@prev.setter
+	def prev(self, node):
+		self.__prev = node
 
 
 class LinkedList:
-    def __init__(self):
-        self.__head = None
-        self.__tail = None
-        self.__size = 0
+	def __init__(self):
+		self.__head = None
+		self.__tail = None
+		self.__size = 0
 
-    def is_empty(self):
-        return self.__head is None
+	@property
+	def size(self):
+		return self.__size
 
-    def insert_start(self, value):
-        new_node = Node(value)
-        new_node.next = self.__head
+	def __len__(self):
+		return self.__size
 
-        if not self.is_empty():
-            self.__head.prev = new_node
-        else:
-            self.__tail = new_node
-        self.__head = new_node
-        self.__size += 1
+	def is_empty(self):
+		return self.__head is None
 
-    def insert_end(self, value):
-        new_node = Node(value)
+	def insert_start(self, value):
+		new_node = Node(value)
+		new_node.next = self.__head
 
-        if self.is_empty():
-            self.__head = self.__tail = new_node
-        else:
-            self.__tail.next = new_node
-            new_node.prev = self.__tail
-            self.__tail = new_node
-        self.__size += 1
+		if not self.is_empty():
+			self.__head.prev = new_node
+		else:
+			self.__tail = new_node
 
-    def insert_any_after(self, value, index):
-        if index < 0 or index > self.__size:
-            raise IndexError("Out of bounds")
-        if index == 0:
-            self.insert_start(value)
-            return
-        if index == self.__size:
-            self.insert_end(value)
-            return
+		self.__head = new_node
+		self.__size += 1
+		return new_node
 
-        new_node = Node(value)
-        mid = self.__size // 2
+	def insert_end(self, value):
+		new_node = Node(value)
 
-        if index <= mid:
-            current = self.__head
-            for _ in range(index - 1):
-                current = current.next
-        else:
-            current = self.__tail
-            for _ in range(self.__size - index):
-                current = current.prev
+		if self.is_empty():
+			self.__head = self.__tail = new_node
+		else:
+			self.__tail.next = new_node
+			new_node.prev = self.__tail
+			self.__tail = new_node
 
-        new_node.next = current.next
-        new_node.prev = current
-        if current.next:
-            current.next.prev = new_node
-        current.next = new_node
+		self.__size += 1
+		return new_node
 
-        self.__size += 1
+	def insert_at(self, value, index):
+		if index < 0 or index > self.__size:
+			raise IndexError("Index out of bounds")
+		if index == 0:
+			return self.insert_start(value)
+		if index == self.__size:
+			return self.insert_end(value)
 
-    def remove(self, index):
-        if self.is_empty():
-            raise IndexError("Remove from empty list")
-        if index < 0 or index >= self.__size:
-            raise IndexError("Index out of bounds")
+		new_node = Node(value)
+		mid = self.__size // 2
 
-        if index == 0:
-            return self.remove_head()
-        if index == self.__size - 1:
-            return self.remove_tail()
+		if index <= mid:
+			current = self.__head
+			for _ in range(index - 1):
+				current = current.next
+		else:
+			current = self.__tail
+			for _ in range(self.__size - index):
+				current = current.prev
 
-        mid = self.__size // 2
-        if index <= mid:
-            current = self.__head
-            for _ in range(index):
-                current = current.next
-        else:
-            current = self.__tail
-            for _ in range(self.__size - index - 1):
-                current = current.prev
+		new_node.next = current.next
+		new_node.prev = current
+		if current.next:
+			current.next.prev = new_node
+		current.next = new_node
 
-        current.prev.next = current.next
-        current.next.prev = current.prev
-        self.__size -= 1
-        return current.value
+		self.__size += 1
+		return new_node
 
-    def remove_head(self):
-        if self.is_empty():
-            raise IndexError("Remove from empty list")
+	def remove(self, index):
+		if self.is_empty():
+			raise IndexError("Remove from empty list")
+		if index < 0 or index >= self.__size:
+			raise IndexError("Index out of bounds")
 
-        removed_value = self.__head.value
-        if self.__head == self.__tail:
-            self.__head = self.__tail = None
-        else:
-            self.__head = self.__head.next
-            self.__head.prev = None
-        self.__size -= 1
-        return removed_value
+		if index == 0:
+			return self.remove_head()
+		if index == self.__size - 1:
+			return self.remove_tail()
 
-    def remove_tail(self):
-        if self.is_empty():
-            raise IndexError("Remove from empty list")
+		mid = self.__size // 2
+		if index <= mid:
+			current = self.__head
+			for _ in range(index):
+				current = current.next
+		else:
+			current = self.__tail
+			for _ in range(self.__size - index - 1):
+				current = current.prev
 
-        removed_value = self.__tail.value
-        if self.__head == self.__tail:
-            self.__head = self.__tail = None
-        else:
-            self.__tail = self.__tail.prev
-            self.__tail.next = None
-        self.__size -= 1
-        return removed_value
+		current.prev.next = current.next
+		current.next.prev = current.prev
+		self.__size -= 1
+		return current.value
 
-    def print_head_to_tail(self):
-        current = self.__head
-        while current:
-            print(current.value, end=", ")
-            current = current.next
-        print("None")
+	def remove_head(self):
+		if self.is_empty():
+			raise IndexError("Remove from empty list")
 
-    def print_tail_to_head(self):
-        current = self.__tail
-        while current:
-            print(current.value, end=", ")
-            current = current.prev
-        print("None")
+		removed_value = self.__head.value
+		if self.__head == self.__tail:
+			self.__head = self.__tail = None
+		else:
+			self.__head = self.__head.next
+			self.__head.prev = None
+
+		self.__size -= 1
+		return removed_value
+
+	def remove_tail(self):
+		if self.is_empty():
+			raise IndexError("Remove from empty list")
+
+		removed_value = self.__tail.value
+		if self.__head == self.__tail:
+			self.__head = self.__tail = None
+		else:
+			self.__tail = self.__tail.prev
+			self.__tail.next = None
+
+		self.__size -= 1
+		return removed_value
+
+	def find(self, value):
+		current = self.__head
+		index = 0
+		while current:
+			if current.value == value:
+				return index
+			current = current.next
+			index += 1
+		return -1
+
+	def __str__(self):
+		values = []
+		current = self.__head
+		while current:
+			values.append(str(current.value))
+			current = current.next
+		return " <-> ".join(values) + " -> None" if values else "Empty list"
 
 
 def main():
@@ -160,18 +176,25 @@ def main():
 	dll.insert_end(30)
 	dll.insert_start(5)
 
-	print("Forward:")
-	dll.print_head_to_tail()
+	print("Forward:", dll)
+	print("Size:", dll.size)
 
-	print("Backward:")
-	dll.print_tail_to_head()
+	print("\nInsert 'Middle' at index 2")
+	dll.insert_at("Middle", 2)
+	print(dll)
 
-	print("\nInsert text at index 2:")
-	dll.insert_any("This is Txet", 2)
-	dll.print_head_to_tail()
-	print("\nRemoved head:", dll.remove_head()), dll.print_head_to_tail()
-	print("\nRemoved tail:", dll.remove_tail()), dll.print_head_to_tail()
-	print("\nRemoved at index 1:", dll.remove(1))
-	dll.print_head_to_tail()
+	print("\nRemoved head:", dll.remove_head())
+	print(dll)
+
+	print("\nRemoved tail:", dll.remove_tail())
+	print(dll)
+
+	print("\nRemoved index 1:", dll.remove(1))
+	print(dll)
+
+	print("\nFind index of 20:", dll.find(20))
+	print("Find index of 999:", dll.find(999))
+	print("Length with len():", len(dll))
+
 
 main()

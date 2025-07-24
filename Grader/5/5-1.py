f1ae85e8c81e7df7b3fb528e6c8d34bdb8784576
bLuEscReEn <theetat.ae@gmail.com>
2025-07-24 16:29:18 +0700

    Linked List Update

---
+++ Grader/5/5-1.py
@@ -0,0 +1,105 @@
+class Node:
+	def __init__(self, value):
+		self.value = value
+		self.next = None
+
+class LinkedList:
+	def __init__(self, new_list: list = None):
+		self.__head = None
+		self.__size = 0
+
+		if new_list is not None:
+			for item in new_list:
+				self.append(item)
+
+	@property
+	def size(self):
+		return self.__size
+
+	@property
+	def is_empty(self):
+		return self.__size == 0
+
+	def append(self, data):
+		new_node = Node(data)
+		if self.__head is None:
+			self.__head = new_node
+		else:
+			current = self.__head
+			while current.next is not None:
+				current = current.next
+			current.next = new_node
+		self.__size += 1
+
+	def remove(self, item):
+		current = self.__head
+		previous = None
+
+		while current is not None:
+			if current.value == item:
+				if previous is None:
+					self.__head = current.next  # Remove head
+				else:
+					previous.next = current.next
+				self.__size -= 1
+				return current.value
+			previous = current
+			current = current.next
+		return None
+
+	def remove_head(self):
+		if self.__head is None:
+			return None
+		data = self.__head.value
+		self.__head = self.__head.next
+		self.__size -= 1
+		return data
+
+	def remove_tail(self):
+		if self.__head is None:
+			return None
+		if self.__head.next is None:
+			data = self.__head.value
+			self.__head = None
+			self.__size -= 1
+			return data
+
+		current = self.__head
+		while current.next.next is not None:
+			current = current.next
+		data = current.next.value
+		current.next = None
+		self.__size -= 1
+		return data
+
+	def rt_list(self):
+		values = []
+		current = self.__head
+		while current is not None:
+			values.append(current.value)
+			current = current.next
+		return values
+
+	def print_list(self):
+		values = []
+		current = self.__head
+		while current is not None:
+			values.append(current.value)
+			current = current.next
+		print(values, end = '')
+  
+def main():
+	nums = list(map(int,(input("Enter the numbers list: ").split())))
+
+	even = LinkedList()
+	odd	 = LinkedList()
+ 
+	for n in nums:
+		even.append(n) if n % 2 == 0 else odd.append(n)
+
+	ans = even.rt_list()
+	ans.extend(odd.rt_list())
+
+	print(f"Rearranged list: {ans}")
+
+main()

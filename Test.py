188c257427e6e577b22636051505fe70f71739dd
bLuEscReEn <theetat.ae@gmail.com>
2025-09-05 21:18:05 +0700

    Upadate AVL

--- Test.py
+++ Test.py
@@ -1,52 +1,32 @@
 class Node:
-	def __init__(self, data = None):
-		self.__data = data
-		self.__next = None
-  
-	@property
-	def next(self) : return self.__next
- 
-	@property
-	def data(self) : return self.__data
- 
-	@next.setter
-	def next(self, new_node) : self.__next = new_node
- 
-	@data.setter
-	def data(self, new_data) : self.__data  = new_data 
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
+def postfix_to_tree(postfix):
+    stack = []
+    for token in postfix:
+        if token.isalnum():  # operand
+            stack.append(Node(token))
+        else:  # operator
+            right = stack.pop()
+            left = stack.pop()
+            node = Node(token)
+            node.left = left
+            node.right = right
+            stack.append(node)
+    return stack[0]  # root of tree
 
-class Queue:
-	def __init__(self):
-		self.__head = None
-		self.__tail	= None
-		self.__size = 0
-  
-	def enqueue(self, new_data):
-		new_node = Node(new_data)
-		if self.__tail:
-			self.__tail.next = new_node
-		else:
-			self.__head = new_node
-		self.__tail = new_node
-		self.__size += 1
-  
-	def dequeue(self):
-		if self.__head is None:
-			raise IndexError("Dequeue from empty queue")
-		removed_data = self.__head.data
-		self.__head = self.__head.next
-		if self.__head is None:
-			self.__tail = None  # Queue is now empty
-		self.__size -= 1
-		return removed_data
+# Example usage
+def print_inorder(node):
+    if node:
+        print('(', end='')
+        print_inorder(node.left)
+        print(node.value, end='')
+        print_inorder(node.right)
+        print(')', end='')
 
-	def	peek(self):
-		if self.__head == None:
-			raise IndexError("Peek from empty queue")
-		return self.__head.data
-
-	def is_empty(self):
-		return self.__size == 0
- 
-	def get_size(self): return self.__size
+expr = "ab+cde+**"
+root = postfix_to_tree(expr)
+print_inorder(root)
